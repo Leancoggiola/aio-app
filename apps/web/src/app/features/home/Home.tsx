2632966e2a9e16eebc1cb850e292c3832837db81
leancoggiola <leandrocoggiola@hotmail.com>
2026-05-18 20:02:43 -0300

    refactor(web): update imports for reorganized layout components

--- apps/web/src/app/features/home/Home.tsx
+++ apps/web/src/app/features/home/Home.tsx
@@ -1,6 +1,24 @@
-import { FC } from 'react';
-import { Button } from '@mantine/core';
+import { Avatar, Group, Paper, Stack, Text } from '@mantine/core';
+import { StarIcon } from '@phosphor-icons/react/dist/ssr';
+
+import type { FC } from 'react';
 
 export const HomePage: FC = () => {
-  return <Button color="primary.2">Click me</Button>;
+  return (
+    <Paper p="2xl">
+      <Group wrap="nowrap" justify="space-between" mb="xl">
+        <Stack gap="2xs">
+          <Text c="primary.5" fz="sm">
+            Buenas Tardes
+          </Text>
+          <Text c="primary.7" fz="1.5rem" fw={600}>
+            Nombre
+          </Text>
+        </Stack>
+        <Avatar size="lg">
+          <StarIcon />
+        </Avatar>
+      </Group>
+    </Paper>
+  );
 };
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -8,8 +8,8 @@ import { AuthProvider } from '@/core/auth';
 import { SWRProvider } from '@/core/providers';
 import { THEME } from '@/theme/config';
 
-import '@mantine/core/styles.css';
-import '@mantine/notifications/styles.css';
+import '@mantine/core/styles.layer.css';
+import '@mantine/notifications/styles.layer.css';
 
 createRoot(document.getElementById('app')!).render(
   <StrictMode>
